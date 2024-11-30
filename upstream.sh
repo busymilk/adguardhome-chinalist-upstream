259a3cc5301b5933c87a8c109eb763911274e496
busymilk <1061017244@qq.com>
2024-11-30 20:49:26 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -15,15 +15,15 @@ fi
 if [[ $IPv4 == "true" ]]; then
 	if [[ $IPv6 == "true" ]]; then
 		echo "$DATE: IPv4 and IPv6 connections both available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://raw.githubusercontent.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/v6.conf > /dev/null 2>&1
 	else
 		echo "$DATE: Only IPv4 connection available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://raw.githubusercontent.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/v4.conf > /dev/null 2>&1
 	fi
 else
 	if [[ $IPv6 == "true" ]]; then
 		echo "$DATE: Only IPv6 connection available."
-		curl -o "/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
+		curl -o "/tmp/default.upstream" https://raw.githubusercontent.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/v6only.conf > /dev/null 2>&1
 	else
 		echo "$DATE: No available network connection was detected, please try again."
 		exit 1
@@ -37,9 +37,9 @@ sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
 sed -i  "s|/114|/]|g" /usr/share/adguardhome.upstream
 cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
 if [[ $IPv4 == "true" ]]; then
-	sed -i "s|114.114.114.114|https://223.5.5.5/dns-query https://1.12.12.12/dns-query https://doh.360.cn/dns-query|g" /usr/share/adguardhome.upstream
+	sed -i "s|114.114.114.114|https://223.5.5.5/dns-query https://1.12.12.12/dns-query https://doh.360.cn/dns-query https://doh.apad.pro/dns-query|g" /usr/share/adguardhome.upstream
 else
-	sed -i "s|114.114.114.114|https://2400:3200:baba::1/dns-query https://dns.ipv6dns.com/dns-query|g" /usr/share/adguardhome.upstream
+	sed -i "s|114.114.114.114|https://2400:3200:baba::1/dns-query https://dns.ipv6dns.com/dns-query https://doh.apad.pro/dns-query|g" /usr/share/adguardhome.upstream
 fi
 echo "$DATE: Cleaning..."
 rm /tmp/*.upstream
