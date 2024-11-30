6b437352a576362663b992ee84492da091593e69
busymilk <1061017244@qq.com>
2024-12-01 03:51:37 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -4,7 +4,7 @@ set -e
 DATE=`date '+%T'`
 
 
-curl -o "/tmp/default.upstream" https://raw.gitmirror.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/v6.conf > /dev/null 2>&1
+curl -o "/tmp/default.upstream" https://raw.gitmirror.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/dns_for_default > /dev/null 2>&1
 
 echo "$DATE: Getting data updates..."
 curl -o /tmp/chinalist.upstream https://raw.gitmirror.com/felixonmars/dnsmasq-china-list/refs/heads/master/accelerated-domains.china.conf
@@ -14,7 +14,8 @@ cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.u
 sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
 sed -i  "s|/114|/]114|g" /usr/share/adguardhome.upstream
 
-sed -i "s|114.114.114.114|https://223.5.5.5/dns-query https://1.12.12.12/dns-query https://doh.360.cn/dns-query https://doh.apad.pro/dns-query|g" /usr/share/adguardhome.upstream
+response=$(curl -s https://raw.gitmirror.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/dns_only_for_china.conf)
+sed -i "s|114.114.114.114|$response|g" /usr/share/adguardhome.upstream
 
 echo "$DATE: Cleaning..."
 rm /tmp/*.upstream
