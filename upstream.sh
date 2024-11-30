e55514393136277d0242a1593a2325096904fee5
busymilk <1061017244@qq.com>
2024-11-30 21:20:15 +0800

    Update upstream.sh
    bug fix

--- upstream.sh
+++ upstream.sh
@@ -32,17 +32,20 @@ fi
 
 echo "$DATE: Getting data updates..."
 curl -o /tmp/chinalist.upstream https://raw.gitmirror.com/felixonmars/dnsmasq-china-list/refs/heads/master/accelerated-domains.china.conf
+
 echo "$DATE: Processing data format..."
-sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
-sed -i  "s|/114|/]|g" /usr/share/adguardhome.upstream
 cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
+sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
+sed -i  "s|/114|/]114|g" /usr/share/adguardhome.upstream
 if [[ $IPv4 == "true" ]]; then
 	sed -i "s|114.114.114.114|https://223.5.5.5/dns-query https://1.12.12.12/dns-query https://doh.360.cn/dns-query https://doh.apad.pro/dns-query|g" /usr/share/adguardhome.upstream
 else
 	sed -i "s|114.114.114.114|https://2400:3200:baba::1/dns-query https://dns.ipv6dns.com/dns-query https://doh.apad.pro/dns-query|g" /usr/share/adguardhome.upstream
 fi
+
 echo "$DATE: Cleaning..."
 rm /tmp/*.upstream
+
 echo "$DATE: Restarting AdGuardHome service..."
 AdGuardHome -s restart
 echo "$DATE: All finished!"
