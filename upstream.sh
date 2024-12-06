80f8e7a2c41d63327836fdda2daaf4df97cdde03
busymilk <1061017244@qq.com>
2024-12-07 00:46:08 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -3,13 +3,13 @@ set -e
 
 DATE=`date '+%T'`
 
-
+echo "$DATE: Getting data updates...1"
 curl -o "/tmp/default.upstream" https://raw.githubusercontent.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/dns_for_default > /dev/null 2>&1
 
-echo "$DATE: Getting data updates..."
+echo "$DATE: Getting data updates...2"
 curl -o /tmp/chinalist.upstream https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/refs/heads/master/accelerated-domains.china.conf
 
-echo "$DATE: Processing data format..."
+echo "$DATE: Processing data format...3"
 cat "/tmp/default.upstream" "/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
 sed -i  "s|server=|[|g" /usr/share/adguardhome.upstream
 sed -i  "s|/114|/]114|g" /usr/share/adguardhome.upstream
@@ -17,7 +17,7 @@ sed -i  "s|/114|/]114|g" /usr/share/adguardhome.upstream
 response=$(curl -s https://raw.githubusercontent.com/busymilk/adguardhome-chinalist-upstream/refs/heads/master/dns_only_for_china.conf)
 sed -i "s|114.114.114.114|$response|g" /usr/share/adguardhome.upstream
 
-echo "$DATE: Cleaning..."
+echo "$DATE: Cleaning...4"
 rm /tmp/*.upstream
 
 echo "$DATE: Restarting AdGuardHome service..."
